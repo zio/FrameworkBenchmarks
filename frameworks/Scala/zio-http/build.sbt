b4fb1aeacdd3f341d551b7f4955e40a1efe59332
Shruti Verma <62893271+ShrutiVerma97@users.noreply.github.com>
2022-04-13 17:59:14 +0530

    Json route added in zio-http (#4)
    * json route added
    
    * json route added
    
    * json route added

--- frameworks/Scala/zio-http/build.sbt
+++ frameworks/Scala/zio-http/build.sbt
@@ -1,8 +1,6 @@
 name := "zio-http"
-
 version := "1.0.0"
 scalaVersion := "2.13.6"
-
 lazy val zhttp = ProjectRef(uri(s"https://github.com/---COMMIT_SHA---"), "zhttp")
 lazy val root  = (project in file("."))
   .settings(
@@ -12,15 +10,12 @@ lazy val root  = (project in file("."))
       Seq(
         "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core"   % "2.9.1",
         "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "2.9.1" % "compile-internal",
-        "com.amazonaws" % "aws-java-sdk" % "1.11.500",
-        // "io.d11"                                 % "zhttp"
       ),
-    testFrameworks += new TestFramework("zio.test.sbt.ZTestFramework")
-  )
-  .dependsOn(zhttp)
-assembly / assemblyMergeStrategy := {
-  case x if x.contains("io.netty.versions.properties") => MergeStrategy.discard
-  case x =>
-    val oldStrategy = (assembly / assemblyMergeStrategy).value
-    oldStrategy(x)
-}
+    testFrameworks += new TestFramework("zio.test.sbt.ZTestFramework"),
+    assembly / assemblyMergeStrategy  := {
+      case x if x.contains("io.netty.versions.properties") => MergeStrategy.discard
+      case x =>
+        val oldStrategy = (assembly / assemblyMergeStrategy).value
+        oldStrategy(x)
+    }
+  ).dependsOn(zhttp)
--- frameworks/Scala/zio-http/src/main/scala/Main.scala
+++ frameworks/Scala/zio-http/src/main/scala/Main.scala
@@ -1,38 +1,58 @@
-import zhttp.http._
 import zhttp.service.{EventLoopGroup, Server}
-import zio.{App, ExitCode, URIO}
-import com.github.plokhotnyuk.jsoniter_scala.macros._
-import com.github.plokhotnyuk.jsoniter_scala.core._
-import io.netty.buffer.Unpooled
-import io.netty.handler.codec.http.HttpHeaderNames
-import io.netty.util.AsciiString
-import zhttp.http.Response
+import zio._
+import zhttp.http._
 import zhttp.service.server.ServerChannelFactory
+import com.github.plokhotnyuk.jsoniter_scala.core._
+import com.github.plokhotnyuk.jsoniter_scala.macros._
 
+import io.netty.util.AsciiString
+
+import java.time.format.DateTimeFormatter
+import java.time.{Instant, ZoneOffset}
 
 case class Message(message: String)
 
 object Main extends App {
-  val message: String                         = "Hello, World!"
+  private val message: String = "Hello, World!"
+  implicit val codec: JsonValueCodec[Message] = JsonCodecMaker.make
 
-  import io.netty.util.CharsetUtil
+  private val plaintextPath = "/plaintext"
+  private val jsonPath      = "/json"
 
-  private val STATIC_PLAINTEXT     = message.getBytes(CharsetUtil.UTF_8)
-  private val STATIC_SERVER_NAME   = AsciiString.cached("zio-http")
-  private val STATIC_PLAINTEXT_LEN = STATIC_PLAINTEXT.length
+  private val STATIC_SERVER_NAME = AsciiString.cached("zio-http")
 
-  private val PLAINTEXT_CLHEADER_VALUE = AsciiString.cached(String.valueOf(STATIC_PLAINTEXT_LEN))
-  // Create HTTP route
-  val app: HttpApp[Any, Nothing] = Http.response(Response.text(message).withServerTime.memoize.addHeader(HttpHeaderNames.SERVER, STATIC_SERVER_NAME))
-  val server = Server.app(app) ++
-    Server.port(8080) ++
-    Server.keepAlive ++
-    Server.disableLeakDetection
+  private val JsonResponse = Response
+    .json(writeToString(Message(message)))
+    .withServerTime
+    .withServer(STATIC_SERVER_NAME)
+    .freeze
 
+  private val PlainTextResponse = Response
+    .text(message)
+    .withServerTime
+    .withServer(STATIC_SERVER_NAME)
+    .freeze
 
+  private def plainTextApp(response: Response) = Http.fromHExit(HExit.succeed(response)).whenPathEq(plaintextPath)
 
-  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
-    server.make.useForever.provideCustomLayer(ServerChannelFactory.auto ++ EventLoopGroup.auto(8)).exitCode
-  }
+  private def jsonApp(json: Response) = Http.fromHExit(HExit.succeed(json)).whenPathEq(jsonPath)
 
+  private def app = for {
+    plainTextResponse <- PlainTextResponse
+    jsonResponse      <- JsonResponse
+  } yield plainTextApp(plainTextResponse) ++ jsonApp(jsonResponse)
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+    app
+      .flatMap(server(_).make.useForever)
+      .provideCustomLayer(ServerChannelFactory.auto ++ EventLoopGroup.auto(8))
+      .exitCode
+
+  private def server(app: HttpApp[Any, Nothing]) =
+    Server.app(app) ++
+      Server.port(8080) ++
+      Server.error(_ => UIO.unit) ++
+      Server.disableLeakDetection ++
+      Server.consolidateFlush ++
+      Server.disableFlowControl
 }
